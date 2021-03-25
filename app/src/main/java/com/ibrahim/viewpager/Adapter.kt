8e693d84963159e3c5542523e0474a65aeca41ef
ibrahim4851 <erogluih2001@gmail.com>
2021-03-25 16:12:41 +0300

    Last Commit

--- app/src/main/java/com/ibrahim/viewpager/Adapter.kt
+++ app/src/main/java/com/ibrahim/viewpager/Adapter.kt
@@ -2,22 +2,24 @@ package com.ibrahim.viewpager
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.NonNull
 import androidx.viewpager.widget.PagerAdapter
 import com.bumptech.glide.Glide
 
-class Adapter : PagerAdapter() {
+class Adapter: PagerAdapter {
 
-    private lateinit var vPagerModel: List<ViewPagerModel>
-    private lateinit var layoutInflater: LayoutInflater
-    private lateinit var context: Context
+    var vPagerModel: List<ViewPagerModel>
+    lateinit var layoutInflater: LayoutInflater
+    var context: Context
 
-    fun Adapter(models: List<ViewPagerModel>, context: Context) {
-        this.vPagerModel = models
+    constructor(model: List<ViewPagerModel>, context: Context): super() {
+        this.vPagerModel = model
         this.context = context
     }
 
@@ -33,15 +35,16 @@ class Adapter : PagerAdapter() {
         layoutInflater = LayoutInflater.from(context)
         val view: View = layoutInflater.inflate(R.layout.viewpageritem, container, false)
 
-        var imageView: ImageView
-        var title: TextView
-        var content: TextView
+        val imageView: ImageView
+        val title: TextView
+        val content: TextView
 
         imageView = view.findViewById(R.id.imageview)
         title = view.findViewById(R.id.title)
         content = view.findViewById(R.id.content)
 
         Glide.with(context).load(vPagerModel.get(position).image).into(imageView)
+        Log.i("imageurl", vPagerModel.get(position).image)
         title.text = vPagerModel.get(position).title
         content.text = vPagerModel.get(position).content
 
@@ -53,6 +56,10 @@ class Adapter : PagerAdapter() {
 
         container.addView(view, 0)
         return view
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
     }
 
 }
--- app/src/main/java/com/ibrahim/viewpager/MainActivity.kt
+++ app/src/main/java/com/ibrahim/viewpager/MainActivity.kt
@@ -2,16 +2,50 @@ package com.ibrahim.viewpager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.viewpager.widget.ViewPager
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewPager : ViewPager
     private lateinit var adapter: Adapter
-    private lateinit var model: List<ViewPagerModel>
+    private lateinit var model: MutableList<ViewPagerModel>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        model = ArrayList<ViewPagerModel>()
+        viewPager = findViewById(R.id.viewPager)
+        adapter = Adapter(model as ArrayList<ViewPagerModel>, applicationContext)
+        viewPager.adapter = adapter
+        otherRequest()
     }
+
+
+    fun otherRequest() {
+        val mQueue: RequestQueue = Volley.newRequestQueue(this)
+        val url = "https://raw.githubusercontent.com/ibrahim4851/ViewPager/master/data.json"
+        val request = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                val jsonArray = response.getJSONArray("sample")
+                for (i in 0 until jsonArray.length()) {
+                    val jsonObject = jsonArray.getJSONObject(i)
+                    val description = jsonObject.getString("description")
+                    val title = jsonObject.getString("title")
+                    val imageUrl = jsonObject.getString("image-url")
+                    val imageModel = ViewPagerModel(title, description, imageUrl)
+                    model.add(imageModel)
+                }
+                Log.i("modellist", model.toString())
+                adapter.notifyDataSetChanged()
+            })
+        { error -> error.printStackTrace() }
+        mQueue.add(request)
+    }
+
 }
